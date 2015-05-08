81f101950181c0d6f6a6d9e55cec8b110c18abcc
Timidfriendly <timid@point-online.org>
2015-05-08 23:41:15 +0200

    1.6 - Not for sale: hide item because store.product.soldOut == true

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,14 @@
-(function(){
-    var gem = { name: 'Azurite', price: 2.95 };
+(function() {
     var app = angular.module('gemStore', []);
-    app.controller('StoreController', function () {
+
+    app.controller('StoreController', function(){
         this.product = gem;
     });
+
+    var gem = {
+        name: 'Azurite',
+        price: 110.50,
+        canPurchase: false,
+        soldOut: true
+    };
 })();
