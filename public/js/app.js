a1066cda3003c74201d6c1eec4adf3f01bb113fc
Timidfriendly <timid@point-online.org>
2015-05-08 23:30:55 +0200

    1.3 & 1.4 & 1.5

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,7 @@
-var app;
-app = angular.module('gemStore', []);
+(function(){
+    var gem = { name: 'Azurite', price: 2.95 };
+    var app = angular.module('gemStore', []);
+    app.controller('StoreController', function () {
+        this.product = gem;
+    });
+})();
