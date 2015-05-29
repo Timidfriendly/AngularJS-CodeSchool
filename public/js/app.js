0c6e16b7dc62cd1a30c8be89e35c409c3502f4f4
Timidfriendly <timid@point-online.org>
2015-05-29 21:18:32 +0200

    1.7 - Look, More Gems! - refinement

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,19 @@
     });
 
     var gems = [
-        { name: 'Azurite', price: 2.95 },
-        { name: 'Bloodstone', price: 5.95 },
-        { name: 'Zircon', price: 3.95 }
+        {
+            name: 'Azurite',
+            price: 2.95
+        },
+
+        {
+            name: 'Bloodstone',
+            price: 5.95
+        },
+
+        {
+            name: 'Zircon',
+            price: 3.95
+        }
     ];
 })();
